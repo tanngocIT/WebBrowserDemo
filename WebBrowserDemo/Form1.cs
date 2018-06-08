62cb1f8e921d790c88fab35d57da298874759ba5
DESKTOP-5N6UGAC\NGOC TAN <tanngocIT@gmail.com>
2018-06-08 19:49:58 +0700

    Add project files.

---
+++ WebBrowserDemo/Form1.cs
@@ -0,0 +1,30 @@
+﻿using CefSharp;
+using CefSharp.WinForms;
+using System.Windows.Forms;
+
+namespace WebBrowserDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitBrowser();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Cef.Shutdown();
+        }
+
+        public ChromiumWebBrowser browser;
+
+        public void InitBrowser()
+        {
+            Cef.Initialize(new CefSettings());
+            browser = new ChromiumWebBrowser("www.google.com");
+            this.Controls.Add(browser);
+            browser.Dock = DockStyle.Fill;
+        }
+    }
+}
